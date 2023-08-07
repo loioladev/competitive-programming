3d8fea6df51d590f516f57e1b173ec8273f86136
Matheus Loiola <matheusloiolapinto@gmail.com>
2023-08-07 17:25:40 -0300

    feat: improved model for competitions

--- model.cpp
+++ model.cpp
@@ -1,7 +1,11 @@
 #include <bits/stdc++.h>
 
-#define LLINF 0x7FFFFFFFFFFFFFFF
-#define INTINF 0x7FFFFFFF
+#define LINF 0x7FFFFFFFFFFFFFFF
+#define INF 0x7FFFFFFF
+#define MOD 1e9+7
+#define sz(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
 
 using namespace std;
 using vi = vector<int>;
@@ -9,13 +13,19 @@ using pii = pair<int, int>;
 using ll = long long;
 using tii = tuple<int, int, int>;
 
-void control(){
+void solve(){
+    
     return;
 }
 
 int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    control();
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int tc = 1;
+    // cin >> tc;
+    for (int t = 1; t <= tc; t++){
+        // cout << "Case #" << t << ": ";
+        solve();
+    }
     return 0;
 }
